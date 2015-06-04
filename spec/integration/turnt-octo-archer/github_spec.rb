bd247abb24f85bd56b92187f1f1bc11292cb659f
Matt McKillip <matt.mckillip@cerner.com>
2015-06-04 14:54:24 -0500

    added test cases for the new instance variables created in initialize

--- spec/integration/turnt-octo-archer/github_spec.rb
+++ spec/integration/turnt-octo-archer/github_spec.rb
@@ -6,6 +6,54 @@ RSpec.describe Github do
     @github = Github.new('http://github.cerner.com/api/v3/repos/', 'OpsInfra', 'ops_spork')
   end
 
+  describe '.id' do
+    it 'returns id' do
+      expect(@github.id).to eq 9524
+    end
+  end
+
+  describe '.name' do
+    it 'returns name' do
+      expect(@github.name).to eq 'ops_spork'
+    end
+  end
+
+  describe '.owner' do
+    it 'returns owner' do
+      expect(@github.owner).to eq 'OpsInfra'
+    end
+  end
+
+  describe '.private?' do
+    it 'returns private?' do
+      expect(@github.private?).to eq false
+    end
+  end
+
+  describe '.url' do
+    it 'returns url' do
+      expect(@github.url).to eq 'http://github.cerner.com/api/v3/repos/OpsInfra/ops_spork'
+    end
+  end
+
+  describe '.created_at' do
+    it 'returns date created_at' do
+      expect(@github.created_at).to eq '2013-10-22T16:09:27Z'
+    end
+  end
+
+  describe '.issues?' do
+    it 'returns issues' do
+      expect(@github.issues?).to eq true
+    end
+  end
+
+  describe '.default_branch' do
+    it 'returns default_branch' do
+      expect(@github.default_branch).to eq 'master'
+    end
+  end
+
   describe '.subscribers' do
     it 'returns subscribers' do
       expect(@github.subscribers).to eq 19
