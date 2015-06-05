43be22ba04bbf7abb2845121cd12e79f93c841c9
Matt McKillip <matt.mckillip@cerner.com>
2015-06-05 14:54:55 -0500

    Now storing data as a hash which allows better storage and display. also added ability to generate html

--- lib/turnt/octo/archer/cli.rb
+++ lib/turnt/octo/archer/cli.rb
@@ -1,36 +1,106 @@
 require 'turnt/octo/archer'
 require 'turnt/octo/archer/git_hub'
+require 'json'
 
 module TurntOctoArcher
   class CLI
+
+    # Public: Initialize a new GitHub wrapper.
+    #
+    # api_url       - The String which will contain the base api url.
+    # org_name      - The String which will contain the organizations name.
+    # project_name  - The String which will contain the project name.
+    # all           - The Boolean variable which will display all information if true.
+    # commits       - The Boolean variable which will display the commit information if true.
+    # issues        - The Boolean variable which will display the issue information if true.
+    # general       - The Boolean variable which will display the general information if true.
+    #
+    # Returns nothing.
     def initialize(options)
-      @api_url = options[:api_url] == nil ? 'http://api.github.cerner.com/repos/' : options[:api_url]
-      @org_name = options[:org_name] == nil ? 'OpsInfra' : options[:org_name]
-      @project_name = options[:project_name] == nil ? 'ops_spork' : options[:project_name]
+      @api_url      = options[:api_url]       == nil ? 'http://api.github.cerner.com/repos/'  : options[:api_url]
+      @org_name     = options[:org_name]      == nil ? 'OpsInfra'                             : options[:org_name]
+      @project_name = options[:project_name]  == nil ? 'ops_spork'                            : options[:project_name]
 
       # Default to false if not given any information
-      @all = options[:all] == nil ? false : options[:all]
-      @commits = options[:commits] == nil ? false : options[:commits]
-      @issues = options[:issues] == nil ? false : options[:issues]
-      @general = options[:general] == nil ? false : options[:general]
+      @all      = options[:all]     == nil ? false : options[:all]
+      @commits  = options[:commits] == nil ? false : options[:commits]
+      @issues   = options[:issues]  == nil ? false : options[:issues]
+      @general  = options[:general] == nil ? false : options[:general]
 
       # if no options are chosen, display everything
       @all = true unless @commits || @issues || @general
     end
 
+    # Public: Run the calls to git_hub.rb and display
+    #
+    #
     def run
       my_github = GitHub.new(@api_url, @org_name, @project_name)
       header(my_github)
-      general_info(my_github)
-      issue_info(my_github)
-      commit_info(my_github)
-      puts @my_string
+      my_hash = build_hash(my_github)
+      puts hash_to_haml(my_hash)
+
+
+      File.open("turnt-octo-archer.html", 'w') { |file| file.write(HashToHTML(my_hash)) }
 
     end
 
+    def build_hash(my_github)
+      git_hub_hash = {}
+      if @all || @general
+        git_hub_hash['general info'] = {}
+        git_hub_hash['general info']['owner'] = my_github.owner
+        git_hub_hash['general info']['id'] = my_github.id
+        git_hub_hash['general info']['created'] = my_github.created_at
+        git_hub_hash['general info']['languages'] = my_github.languages
+        git_hub_hash['general info']['num branches'] = my_github.number_of_branches
+        git_hub_hash['general info']['default branches'] = my_github.default_branch
+        git_hub_hash['general info']['private'] = my_github.private?
+        git_hub_hash['general info']['num subscribers'] = my_github.subscribers
+      end
+
+      if @all || @issues
+        git_hub_hash['issues'] = {}
+        git_hub_hash['issues']['issues?'] = my_github.issues?
+        git_hub_hash['issues']['open issues'] = my_github.current_issues
+      end
+
+      if @all || @commits
+        git_hub_hash['commits'] = {}
+        git_hub_hash['commits']['commits past year'] = my_github.yearly_commits
+        git_hub_hash['commits']['commits per author'] = my_github.commits_per_author
+        git_hub_hash['commits']['commits per day'] = my_github.yearly_commits
+
+        week_arr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+        git_hub_hash['commits']['commits per day'] = {}
+        0.upto(6) do |i|
+          git_hub_hash['commits']['commits per day'][week_arr[i].to_s] = my_github.commits_per_day_of_week(i)
+        end
+
+        git_hub_hash['commits']['commits per hour'] = {}
+        0.upto(23) do |i|
+          git_hub_hash['commits']['commits per hour'][i.to_s] = my_github.commits_per_hour_of_day(i)
+        end
+      end
+      git_hub_hash
+    end
+
+    INDENT = '  ' # use 2 spaces for indentation
+
+    def hash_to_haml(hash, level=0)
+      result = [ "#{INDENT * level}" ]
+      hash.each do |key,value|
+        result << "#{INDENT * (level + 0)} #{key.split.map(&:capitalize)*' '}: #{value}" unless value.is_a?(Hash)
+        result << "#{INDENT * (level + 0)} #{key.split.map(&:capitalize)*' '}" if value.is_a?(Hash)
+        result << hash_to_haml(value, level + 2) if value.is_a?(Hash)
+      end
+      result.join("")
+      result << "\n"
+    end
 
     def header(my_github)
-      @my_string = "
+      my_string = "
                    _                    _                   _                             _
                   | |_ _   _ _ __ _ __ | |_       ___   ___| |_ ___         __ _ _ __ ___| |__   ___ _ __
                   | __| | | | '__| '_ \\| __|____ / _ \\ / __| __/ _ \\ _____ / _` | '__/ __| '_ \\ / _ \\ '__|
@@ -38,58 +108,29 @@ module TurntOctoArcher
                    \\__|\\__,_|_|  |_| |_|\\__|     \\___/ \\___|\\__\\___/       \\__,_|_|  \\___|_| |_|\\___|_|"
 
 
-      @my_string << "\n\nYou are viewing information about #{my_github.url}"
+      my_string << "\n\n\t\t\t\t  You are viewing information about #{my_github.url}"
+      puts my_string
     end
 
-    def general_info(my_github)
-      if @all || @general
-        @my_string << "\n\nRepo Information:"
-        @my_string << "\nOwner: #{my_github.owner}"
-        @my_string << "\nID: #{my_github.id}"
-        @my_string << "\nCreated: #{my_github.created_at}"
-        @my_string << "\nLangauges:"
-        my_github.languages.each{ |language, _| @my_string << " #{language}"}
-        @my_string << "\nNumber of Branches: #{my_github.number_of_branches}"
-        @my_string << "\nDefault Branch: #{my_github.default_branch}"
-        @my_string << "\nPrivate: #{my_github.private?}"
-        @my_string << "\nNumber of Subscribers#{my_github.subscribers}"
-      end
-    end
+    def HashToHTML(hash, opts = {})
+      return if !hash.is_a?(Hash)
 
+      indent_level = opts.fetch(:indent_level) { 0 }
 
-    def issue_info(my_github)
-      if @all || @issues
-        @my_string << "\n\nIssues:"
-        @my_string << "\nOpen Issues: #{my_github.issues?}"
-        if my_github.issues?
-          my_github.current_issues.each do |subarray|
-            @my_string << "\n\n  ID: #{subarray['id']}"
-            @my_string << "\n  Title: #{subarray['title']}"
-            @my_string << "\n  User: #{subarray['user']}"
-            @my_string << "\n  State: #{subarray['state']}"
-            @my_string << "\n  URL: #{subarray['url']}"
-          end
+      out = " " * indent_level + "<ul>\n"
+
+      hash.each do |key, value|
+        out += " " * (indent_level + 2) + "<li><strong>#{key}:</strong>"
+
+        if value.is_a?(Hash)
+          out += "\n" + HashToHTML(value, :indent_level => indent_level + 2) + " " * (indent_level + 2) + "</li>\n"
+        else
+          out += " <span>#{value}</span></li>\n"
         end
       end
+
+      out += " " * indent_level + "</ul>\n"
     end
 
-    def commit_info(my_github)
-      if @all || @commits
-        week_arr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-        @my_string << "\n\nCommit Information:"
-        @my_string << "\nTotal Commits in the Past 4 Weeks: #{my_github.commits_past_weeks(5)}"
-        @my_string << "\nTotal Commits in the Past Year: #{my_github.yearly_commits}"
-        @my_string << "\n  Commits\t  Commiter"
-        my_github.commits_per_author.each { |author, num_commits| @my_string << "\n\t#{num_commits}\t\t\t#{author}"}
-        @my_string << "\n\n  Commits\t  Day of the Week"
-        0.upto(6) do |i|
-          @my_string << "\n\t#{my_github.commits_per_day_of_week(i)}\t\t\t#{week_arr[i]}"
-        end
-        @my_string << "\n\n  Commits\t  Hour of the Day"
-        0.upto(23) do |i|
-          @my_string << "\n\t#{my_github.commits_per_hour_of_day(i)}\t\t\t#{i}"
-        end
-      end
-    end
   end
 end
